6e69faadd15600b949219584c38516094977fd4b
Andrea Borg <andreaborg217@gmail.com>
2026-02-24 16:41:38 +0100

    tasks: move dotfiles clone to pre_ansible

---
+++ pre_ansible.sh
@@ -0,0 +1,20 @@
+
+#!/bin/bash
+
+# INSTALL HOMEBREW
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# INSTALL GIT
+brew install git
+
+# INSTALL ANSIBLE
+brew install ansible
+brew install ansible-lint
+
+# Clone dotfiles
+mkdir -p $HOME/Documents/GitHub/dev_setup
+git clone https://github.com/AndreaBorg217/dev_setup.stow $HOME/Documents/GitHub/dev_setup
+
+
